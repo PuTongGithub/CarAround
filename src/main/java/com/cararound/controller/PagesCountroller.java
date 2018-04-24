68f54f3a35ee30bb22434c62bd02bf4ecab8a98b
PuTongGithub <282549045@qq.com>
2018-04-24 17:25:20 +0800

    package

--- src/main/java/com/cararound/controller/PagesCountroller.java
+++ src/main/java/com/cararound/controller/PagesCountroller.java
@@ -25,63 +25,63 @@ public class PagesCountroller {
 	
 	@GetMapping("/video")
 	public String video() {
-		return "/video/video";
+		return "video/video";
 	}
 	
 	@GetMapping("/fm")
 	public String fm() {
-		return "/fm/fm";
+		return "fm/fm";
 	}
 	
 	@GetMapping("/weather")
 	public String weather() {
-		return "/weather/weather";
+		return "weather/weather";
 	}
 	
 	@GetMapping("/map")
 	public String map() {
-		return "/map/map";
+		return "map/map";
 	}
 	
 	@GetMapping("/news")
 	public String news() {
-		return "/news/news";
+		return "news/news";
 	}
 	
 	@GetMapping("/news-article")
 	public String news_article(String url, Model model) throws Exception {
 		String content = getArticleService.getArticle(url);
 		model.addAttribute("article_content", content);
-		return "/news/news-article";
+		return "news/news-article";
 	}
 	
 	@GetMapping("/music")
 	public String music() {
-		return "/music/music";
+		return "music/music";
 	}
 	
 	@GetMapping("/music-player")
 	public String music_player() {
-		return "/music/music-player";
+		return "music/music-player";
 	}
 	
 	@GetMapping("/music-search")
 	public String music_search() {
-		return "/music/music-search";
+		return "music/music-search";
 	}
 	
 	@GetMapping("/music-list")
 	public String music_list() {
-		return "/music/music-list";
+		return "music/music-list";
 	}
 	
 	@GetMapping("/com")
 	public String com() {
-		return "/com/com";
+		return "com/com";
 	}
 	
 	@GetMapping("/status")
 	public String status() {
-		return "/status/status";
+		return "status/status";
 	}
 }
